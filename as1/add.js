95079152c73fd0e54cf1eb200a95274b7ac7a830
Kanishka Rajput <97453078+KanishkaRajputd@users.noreply.github.com>
2022-03-02 21:00:42 +0530

    hii

---
+++ as1/add.js
@@ -0,0 +1,5 @@
+function add(a,b){
+    console.log(a+b)
+    return a+b ;
+}
+module.exports=add;
---
+++ as1/main.js
@@ -0,0 +1,8 @@
+const x=require("./add");
+const y=require("./divide")
+const z=require("./subtract")
+const k=require("./multiply")
+x(34,44);
+y(34,44);
+z(34,44);
+k(34,44);
